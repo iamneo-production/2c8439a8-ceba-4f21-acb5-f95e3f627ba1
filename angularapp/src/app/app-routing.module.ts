26fc4bf09e845dfdaf8711458c8ed1f1f34cf271
Vasanth S <vasanthselvaraj410@gmail.com>
2023-06-27 17:38:18 +0530

    adminmodule updated

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EditagentsComponent } from './adminmodule/editagents/editagents.component';
+import { AdminmoduleComponent } from './adminmodule/adminmodule.component';
+import { EditpropertyComponent } from './adminmodule/editproperty/editproperty.component';
+import { EditusersComponent } from './adminmodule/editusers/editusers.component';
+import { InquiriesComponent } from './adminmodule/inquiries/inquiries.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent },
+  { path: 'editproperty' , component: EditpropertyComponent},
+  { path: 'editusers' , component: EditusersComponent},
+  { path: 'editagent' , component: EditagentsComponent},
+  { path: 'query' , component: InquiriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
