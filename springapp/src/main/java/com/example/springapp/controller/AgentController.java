1ba7158f71dfc4b75343e3666c32b0f855ed5ba8
Anshi <88302789+Anshi-07@users.noreply.github.com>
2023-07-29 00:27:44 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/example/springapp/controller/AgentController.java
@@ -0,0 +1,59 @@
+package com.example.springapp.controller;
+
+import com.example.springapp.model.Agent;
+import com.example.springapp.service.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agents")
+@CrossOrigin(origins = "https://8081-bddebfabedaecdbaafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io")  //Vasanth
+// @CrossOrigin(origins = "https://8081-deacaeadeadfafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Anshi
+// @CrossOrigin(origins = "https://8081-bfbceacbaecbecafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Lakshana
+// @CrossOrigin(origins = "https://8081-eedfeafbdafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Raas
+// @CrossOrigin(origins = "https://8081-deacaeadeadfafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Suganya
+// @CrossOrigin(origins = "https://8081-dcdbfcfaafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Susmit
+// @CrossOrigin(origins = "https://8081-cfcedadddfafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Subhashini
+// @CrossOrigin(origins = "https://8081-cdcbcbdbddadfacfafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Bhavana
+// @CrossOrigin(origins = "https://8081-dedcbcbaedafaaafbdbcfcbaedbffbeeaadbbb.project.examly.io/")  //Revan
+public class AgentController {
+    @Autowired
+    private AgentService agentService;
+
+    // Get all agents
+    @GetMapping
+    public List<Agent> getAllAgents() {
+        return agentService.getAllAgents();
+    }
+
+    // Create a new agent
+    @PostMapping
+    public Agent createAgent(@RequestBody Agent agent) {
+        return agentService.createAgent(agent);
+    }
+
+    // Get agent by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Agent> getAgentById(@PathVariable long id) {
+        Agent agent = agentService.getAgentById(id);
+        return ResponseEntity.ok(agent);
+    }
+
+    // Update agent by id
+    @PutMapping("/{id}")
+    public ResponseEntity<Agent> updateAgent(@PathVariable long id, @RequestBody Agent agentDetails) {
+        Agent updatedAgent = agentService.updateAgent(id, agentDetails);
+        return ResponseEntity.ok(updatedAgent);
+    }
+
+    // Delete agent by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteAgentById(@PathVariable long id) {
+        agentService.deleteAgentById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ springapp/src/main/java/com/example/springapp/model/LoginModel.java
@@ -0,0 +1,34 @@
+package com.example.springapp.model;
+
+public class LoginModel {
+
+    private String email;
+    private String password;
+
+
+
+
+
+    public LoginModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
